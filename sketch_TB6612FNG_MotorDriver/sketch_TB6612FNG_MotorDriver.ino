b37eb8234619c677b23ace0fc583449596a61382
Garth Martinsen <garth.martinsen@gmail.com>
2023-07-15 12:25:16 -0700

    initial commit of ino sketch for TB6612FNG_Motor_Driver.

---
+++ sketch_TB6612FNG_MotorDriver/sketch_TB6612FNG_MotorDriver.ino
@@ -0,0 +1,24 @@
+#include "Motor.h"
+#include "PwrMgt.h"
+int CW = 0;
+int CCW = 1;
+
+Motor motorA(3,2,4);
+Motor motorB(5,6,7);
+PwrMgt power(8);
+
+void setup() {
+ Serial.begin(115200);
+ while(!Serial);
+ Serial.println("---------Starting-----");
+}
+
+void loop() {
+  Serial.println("Waking up motors..." );
+  power.wakeup();
+  Serial.println("Starting motors: A(3,2,4) and B(5,6,7)" );
+  motorA.run(130, 0, 1200); //(pwm,dir, dur)
+  motorB.run(200, 1, 1200);
+  Serial.println("Putting motors to sleep ..." );
+  power.powerdown();
+}
